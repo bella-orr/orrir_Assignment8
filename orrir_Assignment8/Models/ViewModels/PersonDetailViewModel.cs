eaad545e68e238f37547eeb57906c51b88c2eec9
bella-orr <143103302+bella-orr@users.noreply.github.com>
2025-04-06 21:50:44 -0400

    added the PersonListViewModel and the PersonDetailViewModel

---
+++ orrir_Assignment8/Models/ViewModels/PersonDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace orrir_Assignment8.Models.ViewModels
+{
+    partial class PersonDetailViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private Person person;
+    }
+}
