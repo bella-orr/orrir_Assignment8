589e8c2a89b8457d29166d4a1c1a34b5092ac0a9
bella-orr <143103302+bella-orr@users.noreply.github.com>
2025-04-06 21:40:56 -0400

    added the contact list page

---
+++ orrir_Assignment8/Views/ContactList.xaml.cs
@@ -0,0 +1,14 @@
+namespace orrir_Assignment8.Views;
+
+public partial class ContactList : ContentPage
+{
+	public ContactList()
+	{
+		InitializeComponent();
+	}
+
+    private void Lv_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+
+    }
+}
