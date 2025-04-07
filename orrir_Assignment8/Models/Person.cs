333f7c7bbc0d78a24531ba0f21771a349f24daaf
bella-orr <143103302+bella-orr@users.noreply.github.com>
2025-04-06 21:46:08 -0400

    made a person class

---
+++ orrir_Assignment8/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orrir_Assignment8.Models
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Description { get; set; }
+    }
+}
